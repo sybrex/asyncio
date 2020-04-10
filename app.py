18d06934139c62d6ab96722e4c300ec415b9d01c
Viktor <vsybrex@gmail.com>
2020-04-10 18:47:25 +0300

    Sample

--- app.py
+++ app.py
@@ -1,48 +1,17 @@
-from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
 import asyncio
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def get():
-    return HTMLResponse(html)
+async def get(request: Request):
+    return templates.TemplateResponse('events.html', {'request': request})
 
 
 @app.websocket("/ws")
---
+++ static/js/main.js
@@ -0,0 +1,40 @@
+$(function() {
+    let ws = new WebSocket("ws://localhost:8000/ws");
+    ws.onmessage = function(message) {
+        event = JSON.parse(message.data);
+        if ($('#event_'+event.id).length) {
+            if ($('#time_'+event.id) != event.time) {
+                $('#time_'+event.id).html(event.time);
+            }
+            if ($('#home_'+event.id) != event.markets[0]['bets']['1']) {
+                update_odds($('#home_'+event.id), event.markets[0]['bets']['1'])
+            }
+            if ($('#draw_'+event.id) != event.markets[0]['bets']['X']) {
+                update_odds($('#draw_'+event.id), event.markets[0]['bets']['X'])
+            }
+            if ($('#away_'+event.id) != event.markets[0]['bets']['2']) {
+                update_odds($('#away_'+event.id), event.markets[0]['bets']['2'])
+            }
+            if ($('#state_'+event.id) != event.state) {
+                $('#state_'+event.id).html(event.state);
+            }
+        } else {
+            let row = '<tr id="event_'+event.id+'">'
+                + '<td id="time_'+event.id+'">'+event.time+'</td>'
+                + '<td>'+event.home_team+' - '+event.away_team+'</td>'
+                + '<td id="home_'+event.id+'">'+event.markets[0]['bets']['1']+'</td>'
+                + '<td id="draw_'+event.id+'">'+event.markets[0]['bets']['X']+'</td>'
+                + '<td id="away_'+event.id+'">'+event.markets[0]['bets']['2']+'</td>'
+                + '<td id="state_'+event.id+'">'+event.state+'</td>'
+                + '</tr>'
+            $('#events tbody').append(row);
+        }
+    };
+    function update_odds(element, odds) {
+        if (element.html() != odds) {
+            let color = element.html() > odds ? '#dc3545' : '#28a745';
+            element.css({'background-color': color});
+            element.animate({backgroundColor: '#fff'}, 1000);
+        }
+    }
+});
